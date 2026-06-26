44834f7dfe134cea3e60900270c14bed11641bbf
Kamal <seyedkamalmousavi@gmail.com>
2026-06-26 21:58:04 +0330

    fix render server

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get("/status", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('SKM Server Running on port ${PORT}');
+    console.log(`SKM Server Running on port ${PORT}`);
 });
